d65b18603c327c83bc053f86d24b20203c2bb9c9
Pratham566 <157511299+Pratham566@users.noreply.github.com>
2025-03-24 16:08:13 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-var db = mongoose.connect("mongodb://localhost:27017/user");
+var db = mongoose.connect("mongodb://localhost:27017/user",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 db.then(() => {
   console.log("Connected to mongoDB");
